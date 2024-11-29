5227ca17a1887000714d742663088ceb3be2a0eb
AndresMartinez317 <58409933+AndresMartinez317@users.noreply.github.com>
2024-11-29 02:06:01 -0500

    Delete Assignment2Poo directory

--- Assignment2Poo/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.assignment2poo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.assignment2poo to javafx.fxml;
-    exports org.example.assignment2poo;
-}
